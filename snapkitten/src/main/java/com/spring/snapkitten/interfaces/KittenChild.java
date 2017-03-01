b3c75c620e5975545bfdced566b7d5026f2f7d84
Spring Wong <spring@Springs-MacBook-Air.local>
2017-03-02 01:21:31 +0800

    Added list form of addChilds()

--- snapkitten/src/main/java/com/spring/snapkitten/interfaces/KittenChild.java
+++ snapkitten/src/main/java/com/spring/snapkitten/interfaces/KittenChild.java
@@ -2,6 +2,8 @@ package com.spring.snapkitten.interfaces;
 
 import android.view.View;
 
+import java.util.List;
+
 /**
  * Created by spring on 26/2/2017.
  */
@@ -9,4 +11,5 @@ public interface KittenChild extends KittenBuild {
     KittenChildMethods add(View view);
     KittenChildMethods with(View view);
     KittenChildMethods addChilds(View... views);
+    KittenChildMethods addChilds(List<View> views);
 }
